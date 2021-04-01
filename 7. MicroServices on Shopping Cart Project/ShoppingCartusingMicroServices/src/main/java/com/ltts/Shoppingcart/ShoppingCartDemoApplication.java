9c2a7247c88a7ed4f944c282737348bb48f0d042
stepin104338 <abhishek.vithal@ltts.com>
2021-04-01 10:01:20 +0530

    MicroServices on Shopping Cart

---
+++ 7. MicroServices on Shopping Cart Project/ShoppingCartusingMicroServices/src/main/java/com/ltts/Shoppingcart/ShoppingCartDemoApplication.java
@@ -0,0 +1,31 @@
+package com.ltts.Shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class ShoppingCartDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartDemoApplication.class, args);
+		
+
+		RestTemplate rt = new RestTemplate();
+		WebClient.builder();
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+
+}
